9318a22c729d7a1dc06faa319dda5b16091bc00f
dlsgur2845 <dlsgur2845@naver.com>
2020-08-25 13:11:08 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-sudo apt-get install -y --no-install-recommends bamtools \
+apt-get install -y --no-install-recommends bamtools \
     libbz2-dev \
     liblzma-dev
 
 ### INSTALL HTSLIB 
 wget https://github.com/samtools/htslib/releases/download/1.9/htslib-1.9.tar.bz2
 tar -xvf htslib-1.9.tar.bz2
-cd htslib-1.9.tar.bz2
-sudo ./configure
-sudo make
-sudo make install
+cd htslib-1.9
+./configure
+make
+make install
 cd ..
 
 pip install numpy scipy sklearn tensorflow pandas progressbar2 pysam tensorflow-gpu
